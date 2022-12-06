c7a7fbb0ca30a07a87ccb1da9269c0ef7a17e80b
Valiantsina Ulasava <valiantsina_ulasava@epam.com>
2022-12-06 16:38:28 +0300

    add task stream write

--- src/streams/read.js
+++ src/streams/read.js
@@ -2,9 +2,7 @@ import { createReadStream } from 'node:fs';
 
 const read = async () => {
     const streamReader = createReadStream("./src/streams/files/fileToRead.txt", "utf-8"); 
-    streamReader.on('data', (data) => {
-        process.stdout.write(data);
-    });  
+    streamReader.pipe(process.stdout);
 };
 
 await read();
