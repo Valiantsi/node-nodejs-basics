dcbafe79412ec05004b34057f96671d51ea06545
Valiantsina Ulasava <valiantsina_ulasava@epam.com>
2022-12-06 14:37:32 +0300

    add steams subtask read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,10 @@
+import { createReadStream } from 'node:fs';
+
 const read = async () => {
-    // Write your code here 
+    const streamReader = createReadStream("./src/streams/files/fileToRead.txt", "utf-8"); 
+    streamReader.on('data', (data) => {
+        process.stdout.write(data);
+    });  
 };
 
 await read();
